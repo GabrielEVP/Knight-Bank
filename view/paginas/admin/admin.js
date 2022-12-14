34c3b91cd8e3f66a5986c6417356b8590e0b3ffe
gabriel vargas <gabrielenriquevargas@gmail.com>
2022-12-14 23:38:07 +0100

    navbar_aplication

---
+++ view/paginas/admin/admin.js
@@ -0,0 +1,148 @@
+const btn = $(".menu_btn");
+const menu = document.querySelector(".sidemenu");
+
+btn.click(function(){
+    menu.classList.toggle("menu_expanded");
+    menu.classList.toggle("menu_collapsed");
+
+    document.querySelector('body').classList.toggle("body_expanded")
+  
+})
+
+$(".delete_button").click(function() {
+    $(".modal-bg").css({
+        "visibility": "visible",
+        "background-color": "black",
+        "opacity": "0.7",
+        "transition": "background-color 245ms linear",
+        "transition": "transform 245ms ease"
+    });
+
+    $(".modal-content").css({
+        "visibility": "visible",
+        "transform": "scale(1.0)",
+        "transition": "transform 245ms ease",
+        "z-index": "1000"
+    });
+
+    $(".borrar").css({
+        "display": "block",
+    });
+    
+})
+
+$(".update_button").click(function() {
+    $(".modal-bg").css({
+    "visibility": "visible",
+    "background-color": "black",
+    "opacity": "0.7",
+    "transition": "background-color 245ms linear",
+    });
+
+    $(".modal-content").css({
+        "visibility": "visible",
+        "transform": "scale(1.0)",
+        "transition": "transform 245ms ease",
+        "z-index": "1000"
+    });
+
+    $(".modify").css({
+        "display": "block"
+    });
+})
+
+$(".ban_button").click(function() {
+    $(".modal-bg").css({
+    "visibility": "visible",
+    "background-color": "black",
+    "opacity": "0.7",
+    "transition": "transform 245ms ease",
+    });
+
+    $(".modal-content").css({
+        "visibility": "visible",
+        "transform": "scale(1.0)",
+        "transition": "transform 245ms ease",
+        "z-index": "1000"
+    });
+
+    $(".banear").css({
+        "display": "block"
+    });
+})
+
+$(".desban_button").click(function() {
+    $(".modal-bg").css({
+    "visibility": "visible",
+    "background-color": "black",
+    "opacity": "0.7",
+    "transition": "background-color 245ms linear",
+    });
+
+    $(".modal-content").css({
+        "visibility": "visible",
+        "transform": "scale(1.0)",
+        "transition": "transform 245ms ease",
+        "z-index": "1000"
+    });
+
+    $(".desban").css({
+        "display": "block"
+    });
+})
+
+$("#decline").click(function() {
+    $(".modal-bg").css({
+    "visibility": "hidden"
+    });
+
+    $(".modal-content").css({
+    "visibility": "hidden",
+    "transform": "scale(0)"
+    });
+    
+    $(".borrar").css({
+    "display": "none"
+    });
+
+    $(".modify").css({
+    "display": "none"
+    });
+
+    $(".banear").css({
+        "display": "none"
+    });
+
+    $(".desban").css({
+        "display": "none"
+    });
+})
+
+$("#close").click(function() {
+    $(".modal-bg").css({
+    "visibility": "hidden",
+    });
+
+    $(".modal-content").css({
+    "visibility": "hidden",
+    "transform": "scale(0)"
+    });
+    
+    $(".borrar").css({
+    "display": "none"
+    });
+    
+    $(".modify").css({
+    "display": "none"
+    });
+
+    $(".banear").css({
+        "display": "none"
+    });
+
+    $(".desban").css({
+        "display": "none"
+    });
+
+})
+
