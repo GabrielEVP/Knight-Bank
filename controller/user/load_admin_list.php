58c2cbaf6d5210f3ed2dcdc922ae30a033fcc23c
Josu <j_diez@fpzornotza.com>
2022-12-15 11:27:19 +0100

    añadidas funcionalidades basicas a user list y controllers para obtencion de usuarios

---
+++ controller/user/load_admin_list.php
@@ -0,0 +1,10 @@
+<?php 
+include_once ("model/user/user_list.php"); 
+
+$user_list = new user_list();
+
+$user_list->get_user_list_from_BBDD();
+$inactive_users = $user_list->get_administrators();
+
+echo json_encode($active_users);
+?>
