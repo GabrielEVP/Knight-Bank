dbf78dc3c7ac50aa87f1850a43695c46d40b6d77
josu <j_diez@fpzornotza.com>
2022-12-16 21:44:45 +0100

    funcion cast simplificada, añadidas funciones create_getter y create_setter, añadida funcion getArrayObjVars

--- controller/user/lists/load_user_list.php
+++ controller/user/lists/load_user_list.php
@@ -5,8 +5,8 @@ $user_list = new user_list();
 
 $user_list->get_user_list_from_BBDD();
 print_r($user_list);
-$users = array_map('user_model::getObjvars', $user_list->getObjvars());
-$users = $user_list->getObjvars();
+
+$users = $user_list->getArrayObjVars("user_list");
 
 echo json_encode($users);
 ?>
