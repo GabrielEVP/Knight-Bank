82452aefd742cb30f8f09a7e88371c2f1622b892
Josu <j_diez@fpzornotza.com>
2022-12-16 12:56:00 +0100

    pruebas

--- controller/user/lists/load_user_list.php
+++ controller/user/lists/load_user_list.php
@@ -4,6 +4,8 @@ include_once ("../../../model/user/user_list.php");
 $user_list = new user_list();
 
 $user_list->get_user_list_from_BBDD();
+print_r($user_list);
+$users = array_map('user_model::getObjvars', $user_list->getObjvars());
 $users = $user_list->getObjvars();
 
 echo json_encode($users);
