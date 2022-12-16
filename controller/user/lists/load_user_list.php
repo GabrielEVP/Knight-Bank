69abd78e9eb36bde4e2fc9f4d373d2d9af2922af
gabriel vargas <gabrielenriquevargas@gmail.com>
2022-12-16 23:19:07 +0100

    a

--- controller/user/lists/load_user_list.php
+++ controller/user/lists/load_user_list.php
@@ -3,10 +3,6 @@ include_once ("../../../model/user/user_list.php");
 
 $user_list = new user_list();
 $user_list->get_user_list_from_BBDD();
-<<<<<<< HEAD
-=======
-
->>>>>>> 76e7fdf01695d1ebb88f5c1781f8e879fb35cba1
 $users = $user_list->getArrayObjVars("user_list");
 
 echo json_encode($users);
