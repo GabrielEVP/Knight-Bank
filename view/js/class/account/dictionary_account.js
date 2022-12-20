9380356cc733dd100914af5845f05fc563d30598
josu <j_diez@fpzornotza.com>
2022-12-20 21:15:40 +0100

    mejorado el sistema de rutas de los diccionarios mediante una funcion nueva (controller_path_builder)

--- view/js/class/account/dictionary_account.js
+++ view/js/class/account/dictionary_account.js
@@ -1,19 +1,24 @@
 function controller_url_Account(controller) {
     const url_Account = {
-        load: '/banca/controller/account/load_account.php',
-        new: '/banca/controller/account/new_account.php',
-        modify: '/banca/controller/account/update_account.php',
-        delete: '/banca/controller/account/delete_account.php'
+        load: controller_path_builder("account/load_account.php"),
+        new: controller_path_builder('account/new_account.php'),
+        modify: controller_path_builder('account/update_account.php'),
+        delete: controller_path_builder('account/delete_account.php')
     }
     return url_Account[controller] ?? alert('error url del controller account no existe');
 }
 
 function controller_url_account_List(controller) {
     const url_account_List = {
-        all: '/banca/controller/account/lists/load_account_list.php',
-        load_from_user : '/banca/controller/account/load_accounts_from_user.php',
+        all: controller_path_builder('account/lists/load_account_list.php'),
+        load_from_user : controller_path_builder('account/load_accounts_from_user.php'),
     }
     return url_account_List[controller] ?? alert('error url del controller account no existe');
+}
+
+function controller_path_builder (controller_path) {
+    var path = window.location.pathname;
+    return path.replace("view/js/class/account/dictionary_account.js","") + "/controller/" + controller_path;
 }
 
 export { controller_url_Account , controller_url_account_List }
