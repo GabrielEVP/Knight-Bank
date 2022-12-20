c9848c1829d89cee3291a5b21a5c6546ddcc833e
Josu <j_diez@fpzornotza.com>
2022-12-20 12:55:40 +0100

    adapacion a los cambios en la BBD (id_account añadido), modificaciones en new_account

--- controller/account/new_account.php
+++ controller/account/new_account.php
@@ -6,19 +6,24 @@ include_once ("../../model/user/user_model.php");
 //if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
 $data=json_decode(file_get_contents("php://input"),true);
 
-$account = new account_model();
-$account->generate_new_IBAN();
-
-$user = new user_model();
-$user->setIdUser($data['id_user']);
-$account->setObjUser($user);
-
-$result = array();
-if ($account->insert_account()) {
-    $result['status'] = 'ok';
+if (isset($data['id_user'])) {
+    $account = new account_model();
+    $account->generate_new_IBAN();
+    
+    $user = new user_model();
+    $user->setIdUser($data['id_user']);
+    $account->setObjUser($user);
+    
+    $result = array();
+    if ($account->insert_account()) {
+        $result['status'] = 'ok';
+    } else {
+        $result['status'] = 'error';
+    }
 } else {
-    $result['status'] = 'error';
+    $result['status'] = 'no user';
 }
+
 echo json_encode($result);
 /*} else {
     echo json_encode("no admin");
