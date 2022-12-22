d0096b733a6606d64df3b293661ce59a4b4a6406
Josu <j_diez@fpzornotza.com>
2022-12-22 09:28:22 +0100

    correcciones de cara al servidor

--- view/js/class/user/dictionary_user.js
+++ view/js/class/user/dictionary_user.js
@@ -29,13 +29,15 @@ function controller_path_builder (controller_path) {
     const array_length = path_array.length;
     var erase_path = "";
     for (let i = array_length - 1; i >= 0; i--) {
-        if (path_array[i] == 'banca') {
+        if (path_array[i] == 'view') {
             break;
         } else {
             erase_path = path_array[i] + "/" + erase_path;
         }
     }
+    erase_path = "view/" + erase_path;
     return path.replace(erase_path.slice(0, -1), "") + "controller/" + controller_path;
+    
 }
 
 export { controller_url_User , controller_url_user_List }
