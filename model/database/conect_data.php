a6ecc29ca6a7677268ff5088df82dbceea7d56d0
Gabriel Enrique Vargas Perez <98557334+DarkLouses@users.noreply.github.com>
2023-01-14 21:04:32 +0100

    agregacion de tarjetas, cartas de usuario, y cambios en el modal

--- model/database/conect_data.php
+++ model/database/conect_data.php
@@ -1,10 +1,9 @@
 <?php 
-
 class conect_data {
     public $host='localhost';
-    public $userbbdd='grupo2ze_php';
-    public $passbbdd='1mfr$K72E4@+16';
-    public $ddbbname='grupo2ze_banca';
+    public $userbbdd='root';
+    public $passbbdd='';
+    public $ddbbname='banca';
 }
 
 ?>
