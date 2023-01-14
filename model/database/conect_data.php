026e4d3bd0cb0e6365d95eebe72d33a35827ff7d
josu <j_diez@fpzornotza.com>
2023-01-14 21:11:00 +0100

    cambio de datos de host DDBB

--- model/database/conect_data.php
+++ model/database/conect_data.php
@@ -1,14 +1,14 @@
 <?php 
 class conect_data {
-    public $host='localhost';
-    public $userbbdd='root';
-    public $passbbdd='';
-    public $ddbbname='banca';
-
     // public $host='localhost';
-    // public $userbbdd='grupo2ze_php';
-    // public $passbbdd='1mfr$K72E4@+16';
-    // public $ddbbname='grupo2ze_banca';
+    // public $userbbdd='root';
+    // public $passbbdd='';
+    // public $ddbbname='banca';
+
+    public $host='localhost';
+    public $userbbdd='grupo2ze_php';
+    public $passbbdd='1mfr$K72E4@+16';
+    public $ddbbname='grupo2ze_banca';
 }
 
 ?>
