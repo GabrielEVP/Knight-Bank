e3bab7f597507166260ab3760415380d405b5f19
josu <j_diez@fpzornotza.com>
2023-01-15 21:26:30 +0100

    mejora en la redireccion

--- controller/user/update_profile.php
+++ controller/user/update_profile.php
@@ -6,10 +6,8 @@ session_start();
 
 $root_path = str_replace("controller\user","",__DIR__);
 
-if (! (isset($_POST['name'])) ) {//si no ha mandado el formulario
-    header("Location: " . $root_path . "view\pages\aplication\configuration.html");
-} else if (! (isset($_POST['name'])) ) {//si no esta logeado
-    header("Location: " . $root_path . "index.html");
+if (!(isset($_POST['name'])) || !(isset($_POST['name'])) ) {//si no ha mandado el formulario o no esta loegeado
+    header("Location:".$_SERVER['HTTP_REFERER']);
 } else {
     $response = array();
 
@@ -19,7 +17,7 @@ if (! (isset($_POST['name'])) ) {//si no ha mandado el formulario
     $old_user->setNIF($_SESSION['NIF']);
     $old_user->get_user($old_user->get_id_by_NIF());
     
-    if (isset($_FILES['image'])) {
+    if (isset($_FILES['image']) && $_FILES['image']['size'] > 0) {
         
         $folder_path = $root_path . "view\img\aplication\user\\";
         $path = $folder_path . $old_user->getIdUser() . "_" . $_FILES['image']['name'];
@@ -36,8 +34,9 @@ if (! (isset($_POST['name'])) ) {//si no ha mandado el formulario
         }
     } else {
         $new_user->setFoto($old_user->getFoto());
+        
     }
-    
+   
     if (!isset($response['error'])) {
         $new_user->setIdUser($old_user->getIdUser());
         $new_user->setPassword($old_user->getPassword());
@@ -55,12 +54,10 @@ if (! (isset($_POST['name'])) ) {//si no ha mandado el formulario
 
         $new_user->setFoto(refactor_profile_img_path($new_user->getFoto()));
         $_SESSION['foto'] = $new_user->getFoto();
-        //print_r($_SESSION['foto']);
 
         $response['status'] = 'ok';
     }
-    //print_r($_HOST);
-    header("Location: " . $root_path . "view\pages\aplication\configuration.html");
+    header("Location:".$_SERVER['HTTP_REFERER']);
 }
 
 
