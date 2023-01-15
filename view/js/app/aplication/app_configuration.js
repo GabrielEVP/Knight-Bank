ccdddfe90b63bc52dd283febab86ffb3e5c57b36
Gabriel Enrique Vargas Perez <98557334+DarkLouses@users.noreply.github.com>
2023-01-15 18:16:32 +0100

    navbar_aplication hecho sistema de login mejorado

--- view/js/app/aplication/app_configuration.js
+++ view/js/app/aplication/app_configuration.js
@@ -2,6 +2,7 @@ import { user_class } from "../../class/user/user_class.js";
 import { user_list } from "../../class/user/user_list.js";
 import { controller_url_User } from "../../class/user/dictionary_user.js"
 import { empty_input, show_Modal , quit_Modal } from "../../components/modal.js"
+import { fetch_get_Data } from "../../server/server.js"
 
 const App = angular.module('App', []);
 
@@ -27,6 +28,16 @@ App.controller('Controler', function($scope, $http) {
         }).catch((err) => {
             console.log(err);
         });
+    }
+
+    $scope.logout = async function() {
+        const result = await fetch_get_Data(controller_url_User('logout'));
+        console.log(result);
+        if (result.logout == true) {
+            location.href='../web/login.html';
+        } else {
+            alert('error');
+        }
     }
     
 });
