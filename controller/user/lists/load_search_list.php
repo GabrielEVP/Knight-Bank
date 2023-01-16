1c9825378403b909445d5e4c556af9480cc743b2
Josu <j_diez@fpzornotza.com>
2023-01-16 10:50:07 +0100

    a

--- controller/user/lists/load_search_list.php
+++ controller/user/lists/load_search_list.php
@@ -8,8 +8,9 @@ if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
 
     $user_list = new user_list();
     $user_list->get_user_list_from_BBDD();
-    
+
     $users = $user_list->get_full_extra_list();
+    
     $loged_user = array_search($_SESSION['NIF'], array_column($users, 'NIF'));
     unset($users[$loged_user]); //quitamos el usuario que esta loegeado
 
