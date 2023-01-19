a17277af08fb5a9099f240cf0f513f3a8c14273b
Josu <j_diez@fpzornotza.com>
2023-01-19 12:52:07 +0100

    controller load_financial_data listo

--- controller/account/load_financial_data.php
+++ controller/account/load_financial_data.php
@@ -1,16 +1,16 @@
 <?php
-include_once ("../../model/account/account_model.php"); 
+include_once ("../../model/account/account_list.php"); 
 include_once ("../../model/user/account_move_model.php"); 
 include_once ("../../model/user/user_model.php"); 
 
 session_start();
 
-$account = new account_model();
+$account_list = new account_list();
 $result = array();
 
-$options = array(
+$account_list->get_accounts_from_user_id($_SESSION['id_user']);
+$result['accounts'] = $account_list->getAccountList();
 
-    'period' => "MONTH",
-    'user' => $_SESSION['id_user'],
-);
-$account->get_financial_data();
+$result['financial_data'] = $account_list->get_financial_data();
+
+echo json_encode($result);
