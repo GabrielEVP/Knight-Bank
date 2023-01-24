28e8a0a4383570ac5c4709f69d7640495f454d21
josu <j_diez@fpzornotza.com>
2023-01-24 21:45:13 +0100

    buscador fonetico ajustado a la desaparicion de los modelos list

--- controller/user/lists/load_search_list.php
+++ controller/user/lists/load_search_list.php
@@ -8,14 +8,22 @@ if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
     $data=json_decode(file_get_contents("php://input"),true);
 
     $user_list = new user_model();
-    //$user_list->get_user_list_from_BBDD();
+    $users = $user_list->search_list_from_DDBB($data['search']);
+    $users_id = array_column($users,"id_user");
+    $extra_data_list = $user_list->get_full_extra_list();
 
-    $users = $user_list->get_full_extra_list();
-    
-    $loged_user = array_search($_SESSION['NIF'], array_column($users, 'NIF'));
-    unset($users[$loged_user]); //quitamos el usuario que esta loegeado
+    $final_users = array();
+    foreach ($extra_data_list as $extra_data) {
+        if (array_search($extra_data['id_user'],$users_id) !== false) {
+            array_push($final_users,$extra_data);
+        }
+    }
 
-    echo json_encode(refactor_array_indexes($users));
+    $loged_user = array_search($_SESSION['NIF'], array_column($final_users, 'NIF'));
+    if ($loged_user !== false) {
+        unset($final_users[$loged_user]); //quitamos el usuario que esta logeado
+    }
+    echo json_encode(refactor_array_indexes($final_users));
 } else {
     echo json_encode("no admin");
 }
